b55448c261acccc8b00dc69dd4b62fad148cf910
KnicKnic <knic.knic@gmail.com>
2019-07-07 23:56:57 -0700

    add some more test

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include "../host.h"
 
 using namespace std;
@@ -28,51 +29,6 @@ struct SomeContext {
     std::wstring LoggerContext;
     std::wstring CommandContext;
 };
-
-extern "C" {
-    void Logger(void * context, const wchar_t* s)
-    {
-        auto realContext = (SomeContext*)context;
-        std::wcout << realContext->LoggerContext << std::wstring(s) << L'\n';
-    }
-    void Command(void * context, const wchar_t* s, PowerShellObject *input, unsigned long long inputCount, JsonReturnValues* returnValues)
-    {
-        input; inputCount;
-        auto realContext = (SomeContext*)context;
-        std::wcout << realContext->CommandContext << std::wstring(s) << L'\n';
-
-        for (size_t i = 0; i < inputCount; ++i) {
-            auto& v = input[i];
-            std::wcout << L"In data processing got " << GetToString(v) <<L" of type " << GetType(v) << L'\n';
-        }
-
-        // allocate return object holders
-        returnValues->count = 1 + inputCount;
-        returnValues->objects = (GenericPowershellObject*)malloc(sizeof(*(returnValues->objects)) * returnValues->count);
-        if (returnValues->objects == nullptr) {
-            throw "memory allocation failed for return values in command";
-        }
-
-        // allocate and fill out each object
-        auto& object = returnValues->objects[0];
-        object.releaseObject = char(1);
-        object.type = PowershellObjectTypeString;
-        object.instance.string = MallocCopy(s);
-
-        for (size_t i = 0; i < inputCount; ++i) {
-            auto& v = returnValues->objects[1+i];
-            v.releaseObject = char(0);
-            v.type = PowershellObjectHandle;
-            v.instance.psObject = input[i];
-        }
-
-        return;
-    }
-
-    unsigned char* MallocWrapper(unsigned long long size) {
-        return (unsigned char*)malloc(size);
-    }
-}
 
     template<typename T>
     void FreeFunction(T* ptr) {
@@ -173,11 +129,83 @@ std::wstring GetToString(PowerShellObject handle) {
 
 }
 
+Invoker RunScript(RunspaceHandle& runspace, std::wstring command, bool useLocalScope, const std::vector<std::wstring>& elems) {
+
+    auto powershell = CreatePowershell(runspace);
+    AddScriptSpecifyScope(powershell, command.c_str(), useLocalScope ? 1 : 0);
+    for (auto& arg : elems) {
+        AddArgument(powershell, arg.c_str());
+    }
+
+    return Invoker(powershell);
+}
+Invoker RunScript(RunspaceHandle& runspace, std::wstring command, bool useLocalScope, const std::wstring& elems) {
+    return RunScript(runspace, command, useLocalScope, std::vector<std::wstring>({ elems }));
+}
+Invoker RunScript(RunspaceHandle& runspace, std::wstring command, bool useLocalScope) {
+    return RunScript(runspace, command, useLocalScope, std::vector<std::wstring>({  }));
+}
+Invoker RunScript(RunspaceHandle& runspace, std::wstring command, bool useLocalScope, const std::wstring& elem1, const std::wstring& elem2) {
+    return RunScript(runspace, command, useLocalScope, std::vector<std::wstring>({ elem1, elem2 }));
+}
+RunspaceHandle globalRunspace;
+
+extern "C" {
+    void Logger(void* context, const wchar_t* s)
+    {
+        auto realContext = (SomeContext*)context;
+        std::wcout << realContext->LoggerContext << std::wstring(s) << L'\n';
+    }
+    void Command(void* context, const wchar_t* s, PowerShellObject* input, unsigned long long inputCount, JsonReturnValues* returnValues)
+    {
+        // RunScript(globalRunspace, L"[int]11", true);
+
+        input; inputCount;
+        auto realContext = (SomeContext*)context;
+        std::wcout << realContext->CommandContext << std::wstring(s) << L'\n';
+
+        for (size_t i = 0; i < inputCount; ++i) {
+            auto& v = input[i];
+            std::wcout << L"In data processing got " << GetToString(v) << L" of type " << GetType(v) << L'\n';
+        }
+
+        // allocate return object holders
+        returnValues->count = 1 + inputCount;
+        returnValues->objects = (GenericPowershellObject*)malloc(sizeof(*(returnValues->objects)) * returnValues->count);
+        if (returnValues->objects == nullptr) {
+            throw "memory allocation failed for return values in command";
+        }
+
+        // allocate and fill out each object
+        auto& object = returnValues->objects[0];
+        object.releaseObject = char(1);
+        object.type = PowershellObjectTypeString;
+        object.instance.string = MallocCopy(s);
+
+        for (size_t i = 0; i < inputCount; ++i) {
+            auto& v = returnValues->objects[1 + i];
+            v.releaseObject = char(0);
+            v.type = PowershellObjectHandle;
+            v.instance.psObject = input[i];
+        }
+
+        return;
+    }
+
+    unsigned char* MallocWrapper(unsigned long long size) {
+        return (unsigned char*)malloc(size);
+    }
+}
+
+
 int main()
 {
     InitLibrary(MallocWrapper, free);
     SomeContext context{ L"MyLoggerContext: ", L"MyCommandContext: " };
     auto runspace = CreateRunspace(&context, Command, Logger);
+    globalRunspace = runspace;
+    // RunScript(runspace, L"[int]12", true);
+
     auto powershell = CreatePowershell(runspace);
     //AddScriptSpecifyScope(powershell, L"c:\\code\\psh_host\\script.ps1", 1);
     //AddCommand(powershell, L"c:\\code\\go-net\\t3.ps1");
