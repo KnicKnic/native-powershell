49682db0fb0c7b587455555ab9e3ba3915021105
KnicKnic <knic.knic@gmail.com>
2019-08-01 08:50:05 -0700

    start adding unit tests

--- test_unit/test_unit.cpp
+++ test_unit/test_unit.cpp
@@ -1,3 +1,7 @@
+// test_unit.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
 // test.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
@@ -5,6 +9,10 @@
 #include <algorithm>
 #include <vector>
 #include "../host.h"
+
+//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER // creating own main to do powershell native init
+#include "./Catch2/single_include/catch2/catch.hpp"
 
 using namespace std;
 
@@ -28,48 +36,50 @@ const wchar_t* MallocCopy(const wchar_t* str)
 struct SomeContext {
     std::wstring LoggerContext;
     std::wstring CommandContext;
+    RunspaceHandle runspace;
+    PowershellHandle *powershell = nullptr;
 };
 
-    template<typename T>
-    void FreeFunction(T* ptr) {
-        free((void *)ptr);
-    }
+template<typename T>
+void FreeFunction(T* ptr) {
+    free((void*)ptr);
+}
 
 template<typename T>
 class ZeroResetable {
-	T var;
+    T var;
 public:
-	ZeroResetable() : var(T{ 0 }) {}
-	ZeroResetable(ZeroResetable&& rhs) : var(rhs.var) {
-		rhs.var = T{ 0 };
-	}
-	ZeroResetable& operator=(ZeroResetable&& rhs) {
-		var = rhs.var;
-		rhs.var = T{ 0 };
-		return *this;
-	}
-	ZeroResetable& operator=(T& rhs) {
-		var = rhs;
-		return *this;
-	}
-	ZeroResetable& operator=(T&& rhs) {
-		var = rhs;
-		return *this;
-	}
-	~ZeroResetable() {
-		var = T{ 0 };
-	}
-	operator T& () { return var; }
-	operator const T& ()const { return var; }
-	T& get() { return var; }
-	template<typename Y>
-	auto operator[](Y i) {
-		return var[i];
-	}
-	T* operator->() { return &var; }
-	auto operator*() { return *var; }
-	T* operator&() { return &var; }
-	const bool operator!=(T t) const{ return var != t; }
+    ZeroResetable() : var(T{ 0 }) {}
+    ZeroResetable(ZeroResetable&& rhs) : var(rhs.var) {
+        rhs.var = T{ 0 };
+    }
+    ZeroResetable& operator=(ZeroResetable&& rhs) {
+        var = rhs.var;
+        rhs.var = T{ 0 };
+        return *this;
+    }
+    ZeroResetable& operator=(T& rhs) {
+        var = rhs;
+        return *this;
+    }
+    ZeroResetable& operator=(T&& rhs) {
+        var = rhs;
+        return *this;
+    }
+    ~ZeroResetable() {
+        var = T{ 0 };
+    }
+    operator T& () { return var; }
+    operator const T& ()const { return var; }
+    T& get() { return var; }
+    template<typename Y>
+    auto operator[](Y i) {
+        return var[i];
+    }
+    T* operator->() { return &var; }
+    auto operator*() { return *var; }
+    T* operator&() { return &var; }
+    const bool operator!=(T t) const { return var != t; }
 };
 
 #define DENY_COPY(T) T(T&)=delete ; T& operator=(T&) = delete;
@@ -80,9 +90,9 @@ public:
     Invoker(PowershellHandle handle) {
         exception = InvokeCommand(handle, &objects, &count);
     }
-	DENY_COPY(Invoker);
-	DEFAULT_MOVE(Invoker);
-	//Invoker(Invoker&) = delete; Invoker& operator=(Invoker&) = delete;
+    DENY_COPY(Invoker);
+    DEFAULT_MOVE(Invoker);
+    //Invoker(Invoker&) = delete; Invoker& operator=(Invoker&) = delete;
     ~Invoker() {
         for (unsigned int i = 0; i < count; ++i) {
             ClosePowerShellObject(objects[i]);
@@ -94,21 +104,21 @@ public:
         ClosePowerShellObject(exception);
     }
     const bool CallFailed() const {
-        return exception.operator!=( PowershellHostEmptyHandleValue);
+        return exception.operator!=(PowershellHostEmptyHandleValue);
     }
-	PowerShellObject operator[](unsigned int i) {
-		return objects[i];
-	}
-	PowerShellObject* results() {
-		return objects;
-	}
-	ZeroResetable< PowerShellObject*> objects;
-	ZeroResetable< unsigned int> count;
-	ZeroResetable< PowerShellObject> exception;
+    PowerShellObject operator[](unsigned int i) {
+        return objects[i];
+    }
+    PowerShellObject* results() {
+        return objects;
+    }
+    ZeroResetable< PowerShellObject*> objects;
+    ZeroResetable< unsigned int> count;
+    ZeroResetable< PowerShellObject> exception;
 private:
 };
 
-std::wstring CopyAndFree(const wchar_t * cStr) {
+std::wstring CopyAndFree(const wchar_t* cStr) {
     if (cStr == nullptr) {
         throw std::exception(__FUNCTION__  " got nullptr");
     }
@@ -118,7 +128,7 @@ std::wstring CopyAndFree(const wchar_t * cStr) {
 }
 
 std::wstring GetType(PowerShellObject handle) {
-    if('\0' == IsPSObjectNullptr(handle))
+    if ('\0' == IsPSObjectNullptr(handle))
         return CopyAndFree(GetPSObjectType(handle));
     return L"nullptr";
 }
@@ -129,7 +139,7 @@ std::wstring GetToString(PowerShellObject handle) {
 
 }
 
-Invoker RunScript(RunspaceHandle& runspace, PowershellHandle * parent, std::wstring command, bool useLocalScope, const std::vector<std::wstring>& elems) {
+Invoker RunScript(RunspaceHandle & runspace, PowershellHandle * parent, std::wstring command, bool useLocalScope, const std::vector<std::wstring> & elems) {
 
     PowershellHandle powershell;
     if (parent != nullptr) {
@@ -147,17 +157,15 @@ Invoker RunScript(RunspaceHandle& runspace, PowershellHandle * parent, std::wstr
     DeletePowershell(powershell);
     return results;
 }
-Invoker RunScript(RunspaceHandle& runspace, PowershellHandle* parent, std::wstring command, bool useLocalScope, const std::wstring& elems) {
+Invoker RunScript(RunspaceHandle & runspace, PowershellHandle * parent, std::wstring command, bool useLocalScope, const std::wstring & elems) {
     return RunScript(runspace, parent, command, useLocalScope, std::vector<std::wstring>({ elems }));
 }
-Invoker RunScript(RunspaceHandle& runspace, PowershellHandle* parent, std::wstring command, bool useLocalScope) {
+Invoker RunScript(RunspaceHandle & runspace, PowershellHandle * parent, std::wstring command, bool useLocalScope) {
     return RunScript(runspace, parent, command, useLocalScope, std::vector<std::wstring>({  }));
 }
-Invoker RunScript(RunspaceHandle& runspace, PowershellHandle* parent, std::wstring command, bool useLocalScope, const std::wstring& elem1, const std::wstring& elem2) {
+Invoker RunScript(RunspaceHandle & runspace, PowershellHandle * parent, std::wstring command, bool useLocalScope, const std::wstring & elem1, const std::wstring & elem2) {
     return RunScript(runspace, parent, command, useLocalScope, std::vector<std::wstring>({ elem1, elem2 }));
 }
-RunspaceHandle globalRunspace;
-PowershellHandle * globalPowershell = nullptr;
 
 extern "C" {
     void Logger(void* context, const wchar_t* s)
@@ -174,7 +182,7 @@ extern "C" {
 
         for (size_t i = 0; i < inputCount; ++i) {
             // test nested creation
-            RunScript(globalRunspace, globalPowershell, L"[int]11", true);
+            RunScript(realContext->runspace, realContext->powershell, L"[int]11", true);
             auto& v = input[i];
             std::wcout << L"In data processing got " << GetToString(v) << L" of type " << GetType(v) << L'\n';
         }
@@ -207,13 +215,38 @@ extern "C" {
     }
 }
 
+// declaring at global scope to make testing for validateContext simpler
+unsigned long long validateContext_contextValue = 94847573123111;
+bool validateContext_validatedLogger = false;
+bool validateContext_validatedCallback = false;
 
-int main()
-{
-    InitLibrary(MallocWrapper, free);
+TEST_CASE("validate context") {
+    auto logger = +[](void* context, const wchar_t* s) {validateContext_validatedLogger = true;  REQUIRE(context != nullptr); REQUIRE((unsigned long long)context == validateContext_contextValue); };
+    auto command = +[](void* context, const wchar_t* s, PowerShellObject * input, unsigned long long inputCount, JsonReturnValues * returnValues)
+        {validateContext_validatedCallback = true;  REQUIRE(context != nullptr); REQUIRE((unsigned long long)context == validateContext_contextValue); };
+
+    auto runspace = CreateRunspace((void *)validateContext_contextValue, command, logger);
+
+    auto powershell = CreatePowershell(runspace);
+
+    REQUIRE(0 == AddScriptSpecifyScope(powershell, L"write-host 5;send-hostcommand -message \"hi\"  ", FALSE));
+    
+    Invoker invoke(powershell);
+    REQUIRE(validateContext_validatedLogger);
+    REQUIRE(validateContext_validatedCallback);
+}
+TEST_CASE("nested runspace"){
     SomeContext context{ L"MyLoggerContext: ", L"MyCommandContext: " };
     auto runspace = CreateRunspace(&context, Command, Logger);
-    globalRunspace = runspace;
+    context.runspace = runspace;
+}
+
+
+TEST_CASE("tests", "[native-powershell]")
+{
+    SomeContext context{ L"MyLoggerContext: ", L"MyCommandContext: " };
+    auto runspace = CreateRunspace(&context, Command, Logger);
+    context.runspace = runspace;
     RunScript(runspace, nullptr, L"[int12", true);
 
     auto powershell = CreatePowershell(runspace);
@@ -222,7 +255,7 @@ int main()
     //AddScriptSpecifyScope(powershell, L"write-host $pwd", 0);
     AddScriptSpecifyScope(powershell, L"0;1;$null;dir c:\\", 1);
 
-	//AddCommandSpecifyScope(powershell, L"..\\..\\go-net\\t3.ps1", 0);
+    //AddCommandSpecifyScope(powershell, L"..\\..\\go-net\\t3.ps1", 0);
     //AddScriptSpecifyScope(powershell, L"$a = \"asdf\"", 0);
     //AddArgument(powershell, L"c:\\ddddddd");
     {
@@ -233,22 +266,22 @@ int main()
             wcout << L"Got type: " << GetType(invoke[i]) << L"with value: " << GetToString(invoke[i]) << L'\n';
         }
 
-		auto powershell2 = CreatePowershell(runspace);
+        auto powershell2 = CreatePowershell(runspace);
 
-		// note below will write to output, not return objects	
-		AddScriptSpecifyScope(powershell2, 
+        // note below will write to output, not return objects	
+        AddScriptSpecifyScope(powershell2,
             L"write-host 'about to enumerate directory';"
             L"write-host $args; $len = $args.length; write-host \"arg count $len\";"
             L"$args | ft | out-string | write-host;"
             L"@(1,'asdf',$null,$false) | send-hostcommand -message 'I sent the host a command' | write-host;"
             L"send-hostcommand -message 'I sent the host a command' | write-host", 0);
-		AddArgument(powershell2, L"String to start");
-		AddPSObjectArguments(powershell2, invoke.objects, invoke.count);
-		AddArgument(powershell2, L"String to end");
+        AddArgument(powershell2, L"String to start");
+        AddPSObjectArguments(powershell2, invoke.objects, invoke.count);
+        AddArgument(powershell2, L"String to end");
 
-        globalPowershell = &powershell2;
-		Invoker invoke2(powershell2);
-        globalPowershell = nullptr;
+        context.powershell = &powershell2;
+        Invoker invoke2(powershell2);
+        context.powershell = nullptr;
     }
     DeletePowershell(powershell);
 
@@ -265,7 +298,18 @@ int main()
     DeletePowershell(powershell);
 
     DeleteRunspace(runspace);
-    std::cout << "Hello World!\n"; 
+    std::cout << "Hello World!\n";
+}
+
+int main(int argc, char* argv[]) {
+    // global setup...
+
+    InitLibrary(MallocWrapper, free);
+    int result = Catch::Session().run(argc, argv);
+
+    // global clean-up...
+
+    return result;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -278,3 +322,6 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
